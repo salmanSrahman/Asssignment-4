217627d047be6fe8cda70635c4b239382206138d
Salman-S-Rahman <salmanmern@gmail.com>
2023-08-29 09:36:31 +0600

    server creation & route set up complete in nodejs

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-
   if (req.url == "/") {
     const data = fs.readFileSync("home.html");
     if (data) {
@@ -10,27 +9,29 @@ const server = http.createServer((req, res) => {
       res.write(data);
       res.end();
     }
-  }
-  else if (req.url == "/about") {
+  } else if (req.url == "/about") {
     const data = fs.readFileSync("about.html");
     if (data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
     }
-  }
-  if (req.url == "/") {
-    const data = fs.readFileSync("home.html");
+  } else if (req.url == "/contact") {
+    const data = fs.readFileSync("contact.html");
+    if (data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  } else if (req.url == "/file-write") {
+    const data = fs.writeFileSync("demo.txt", "hello world");
     if (data) {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       res.end();
     }
   }
-
-
-
 });
 
-server.listen(5000);
-console.log("Server listening on port 5000");
+server.listen(3000);
+console.log("Server listening on port 3000");
